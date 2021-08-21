88ea7305764fb158efb2999d04f34e095a6497c1
Jyotiraditya Prasad Dash <dashjyotiraditya@gmail.com>
2021-08-21 17:24:52 +0530

    Initial commit

---
+++ src/main/java/com/bhainas/backend/model/User.java
@@ -0,0 +1,83 @@
+package com.bhainas.backend.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+
+    public User() {
+    }
+
+    public User(UserData ud) {
+        this.firstName = ud.getFirstName();
+        this.lastName = ud.getLastName();
+        this.email = ud.getEmail();
+        this.password = ud.getPassword();
+    }
+
+    public User(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/bhainas/backend/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.bhainas.backend.repository;
+
+import java.util.Optional;
+
+import com.bhainas.backend.model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface UserRepository extends JpaRepository<User,Long>{
+    Optional<User> findByEmail(String email);
+    Boolean existsByEmail(String email);
+    
+}
